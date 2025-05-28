52d845398998b41353896887b24b65478eeb3bed
Raju <ashfaqraju27@gmail.com>
2025-05-29 00:42:41 +0600

    backend launces url setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const path= require("path");
 const express = require('express'); // Added express module
 const cors = require('cors'); // Added cors module
-const planetsRouter = require('./routes/planets/planets.router.js')
+const morgan = require('morgan');
 
+const planetsRouter = require('./routes/planets/planets.router.js')
+const launchesRouter = require('./routes/launches/launches.router.js')
 
 
 
@@ -11,6 +13,7 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000', // Allow requests from this origin
 }));
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,8 +21,13 @@ app.use(express.static(path.join(__dirname, '..','public' )))
 
 
 //importing the planets router
-app.use(planetsRouter)
-app.get('/',(req,res)=>{
+app.use(planetsRouter);
+app.use(launchesRouter);
+
+
+
+//making a default front-end page web-site when it deployed/build
+app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,'..', 'public', 'index.html'))
 })
 
