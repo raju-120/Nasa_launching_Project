538cad1f924ed1cca7cd21590af626511834b13c
Raju <ashfaqraju27@gmail.com>
2025-11-29 17:27:29 +0600

    mongo Schema added

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,41 @@
+const mongoose= require("mongoose");
+
+const launchesSchema = mongoose.Schema({
+    flightNumber: {
+        type: Number,
+        required : true,
+    },
+    launchDate: {
+        type: Date,
+        required: true,
+    },
+    mission:{
+        type: String,
+        required: true,
+    },
+    rocket:{
+        type: String,
+        required: true,
+    },
+    target: {
+        type: String,
+        required: true,
+    },
+    customers: {
+        type: [String],
+        required: true,
+    }
+    ,
+    upcoming: {
+        type: Boolean,
+        required: true,
+    },
+    success: {
+        type: Boolean,
+        required: true,
+        default: true,
+    }
+});
+
+// Connects launches Schema with the "Launches" collection
+module.exports=mongoose.model('Launch', launchesSchema);
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,35 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require("mongoose");
+
 
 const {loadPlanetsData} = require("./models/planet.model.js")
 
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer(app)
+//MongoDB Connection
+const MONGO_URL= "mongodb+srv://nasa-api:Go7Dgi10jfOahX4M@nasacluster.shoyjnu.mongodb.net/?appName=NASACluster"
+
+const server = http.createServer(app);
+
+// testing the DB is connect or not
+mongoose.connection.once('open',()=>{
+    console.log("MONGODB Connection is ready.")
+});
+
+mongoose.connection.on('error', (err)=>{
+    console.error("MongoDb is not connect: ",err.message);
+})
 
 async function startServer(){
+    //connect the DB
+    await mongoose.connect(MONGO_URL,{
+        // Those are options in the MONGODB Drivers.
+        useNewUrlParser: true,
+        userFindAndModify: false,
+        useCreateUIndex: true,
+        useUnifiedTopology: true,
+    });
     await loadPlanetsData();
 
     server.listen(PORT, ()=>{
